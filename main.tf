cd648189cffcbe4106aa0b1682006a74415f0f6b
Sean Williams <williamssean@google.com>
2024-11-11 15:42:45 -0600

    Adding back the TF code that enables the apis.

--- main.tf
+++ main.tf
@@ -22,12 +22,12 @@ provider "google" {
 
 # enable the required GCP services
 # disabled for testing
-# resource "google_project_service" "gcp_services" {
-#   for_each = toset(var.gcp_service_list)
-#   project = var.project_id
-#   service = each.key
-#    disable_dependent_services = true
-# }
+resource "google_project_service" "gcp_services" {
+  for_each = toset(var.gcp_service_list)
+  project = var.project_id
+  service = each.key
+   disable_dependent_services = true
+}
 
 # ** SA for the Connector **
 # Create a new Service Account for analytics role
